d58f874f72e962e15eb03ffbb396dae26c70a1ca
josephmalete1997 <josephmalete75@gmail.com>
2025-09-12 04:38:26 -0400

    Added user registration backend integration

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+//import { Routes, Route, useNavigate } from 'react-router-dom';//original
+import { Routes, Route } from 'react-router-dom';//original
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
@@ -8,7 +9,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  const navigate = useNavigate();
+  //const navigate = useNavigate(); =
   return (
         <>
         <Navbar/>
